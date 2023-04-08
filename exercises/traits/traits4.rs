aa9ec56cf5cc67903ecd4c9aa4983b5fc1d7cd33
Oriol <ocastro@gigapipe.com>
2023-04-08 18:01:06 +0200

    Solve generics and traits exercises

--- exercises/traits/traits4.rs
+++ exercises/traits/traits4.rs
@@ -4,8 +4,6 @@
 // Don't change any line other than the marked one.
 // Execute `rustlings hint traits4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub trait Licensed {
     fn licensing_info(&self) -> String {
         "some information".to_string()
@@ -20,7 +18,8 @@ impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn compare_license_types(software: ??, software_two: ??) -> bool {
+// INstead of specifing a series of possible types, we just indicate that the parameters for the funciton must have the Licensed trait.
+fn compare_license_types(software: impl Licensed, software_two: impl Licensed) -> bool {
     software.licensing_info() == software_two.licensing_info()
 }
 
